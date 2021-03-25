ccbd32e7b3e516c4767d30eb8014d6631d2e30a0
Paulo Victor <pvictorcampelo@yahoo.com.br>
2021-03-25 16:13:52 -0300

    Update do arquivo Tocando_MP3.py

--- Lista_Exercicios_Propostos/Tocando_MP3.py
+++ Lista_Exercicios_Propostos/Tocando_MP3.py
@@ -1,6 +1,12 @@
-import pygame
-pygame.init()
-pygame.mixer.music.load('musica.mp3')
-pygame.mixer.music.play()
-pygame.event.wait()
+from pygame import mixer
+mixer.init()
+mixer.music.load('musica.mp3')
+mixer.music.play()
+input('Solta o som dj')
+'''
+import os, time
 
+os.startfile(r'musica.mp3')
+
+time.sleep(10)
+'''
